9561d7b8017b4d6b8b7719a7ae6e55c94a04ecf3
Hugo Boyce <hugo.boyce.4@gmail.com>
2021-01-06 23:43:34 -0500

    Added the UART1 DMA "job" buffer
    The fct "push_UART1" will start a DMA transfer if the DMA channel is free, of will put it in a buffer to transmit later automatically if the DMA is already busy w/ a tranfer.

---
+++ User/interrupt.h
@@ -0,0 +1,16 @@
+/*
+ * interrupt.h
+ *
+ *  Created on: Jan 6, 2021
+ *      Author: Hugo Boyce
+ */
+
+#ifndef INTERRUPT_H_
+#define INTERRUPT_H_
+
+#include "NuMicro.h"
+#include "user_sys.h"
+
+void PDMA_IRQHandler(void);
+
+#endif /* INTERRUPT_H_ */
